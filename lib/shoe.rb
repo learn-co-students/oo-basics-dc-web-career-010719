f9f880f9c0bc6b1e9fb6c091306165e4a8e2114b
github username <metelskyi88@gmail.com>
2019-01-09 16:23:02 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,32 @@
 # Make your shoe class here!
+class Shoe
+
+def initialize(brand)
+  @brand = brand
+end
+
+attr_accessor:brand #getter/setter
+attr_accessor:color
+attr_accessor:size
+
+def material=(material)
+  @material = material
+end
+def material
+  @material
+end
+
+def condition=(condition) #setter
+  @condition = condition
+end
+
+def condition #getter
+  @condition
+end
+
+def cobble
+  puts "Your shoe is as good as new!"
+  @condition = 'new'
+end
+
+end
